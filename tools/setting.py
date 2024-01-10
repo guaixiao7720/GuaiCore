ec59af272187134f9d41e79715145d37872eaeb0
guaixiao <109229170+guaixiao7720@users.noreply.github.com>
2024-01-10 18:10:29 +0800

    Update setting.py

--- tools/setting.py
+++ tools/setting.py
@@ -10,7 +10,7 @@ def load(game: str):
     """读取GuaiCore设置文件"""
 
     # 后面需要修改 当前仅测试用
-    setting = 打开(f"{PATH}setting/{game}.json", "r")
+    setting = open(f"{PATH}setting/{game}.json", "r")
 
     str1 = setting.readlines()[0]
     setting.close()
@@ -18,7 +18,7 @@ def load(game: str):
     return json.loads(str1)
 
 def load_fonts():
-    fonts_setting = 打开(f"{PATH}setting/fonts.json", "r")
+    fonts_setting = open(f"{PATH}setting/fonts.json", "r")
 
     str1 = fonts_setting.readlines()[0]
     fonts_setting.close()
@@ -30,7 +30,7 @@ def load_fonts():
 def is_first_run():
     """检查引擎是否是第一次运行"""
     # 通过查看setting\hello.txt 的内容判断 请勿随便修改
-    setting = 打开(f"..\\setting\\hello.txt", "r+")
+    setting = open(f"..\\setting\\hello.txt", "r+")
     if setting.readlines()[0] == "True":
         setting.writelines(["False"])
         setting.close()
